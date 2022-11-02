c6dc3fe708af0529850507344bc79eb9d769f270
Lavanya SK <lavanya.sk@updaptcsr.com>
2022-11-02 10:47:53 +0530

    login-page-design-fix

--- src/features/Login/components/Login.js
+++ src/features/Login/components/Login.js
@@ -8,12 +8,21 @@ function Login({ handleSubmit, handleEleChange, formEle }) {
         <div className="login-container-img-wrapper">
           <img className="login-img" src={images.loginImage} alt="Login-img" />
         </div>
-        <div className="login-container-form-wrapper">
+        <div className="login-container-content-wrapper">
           <div className="login-container-inner-wrapper">
-            <h1 className="login-title">LMS</h1>
-            <form className="login-form">
-              <h3 className="login-title">Login</h3>
+            <div className="login-container-heading">
+              <h1 className="login-container-heading-content">
+                Leave Management System
+              </h1>
+            </div>
+            <div className="login-container-form-title">
+              <h3 className="form-title">Login</h3>
+            </div>
+
+            <form className="login-container-form">
+              {/* <div className="login-container-form-wrapper"> */}
               <div className="login-form-username-container">
+                <label className="form-label">Username</label>
                 <input
                   onChange={(e) => {
                     handleEleChange(e, "username");
@@ -21,18 +30,38 @@ function Login({ handleSubmit, handleEleChange, formEle }) {
                   type="text"
                   value={formEle.username}
                   name="username"
-                  placeholder="username"
+                  placeholder="John123"
+                  className="form-textbox"
                 />
               </div>
               <div className="login-form-password-container">
+                <label className="form-label">Password</label>
                 <input
                   onChange={(e) => {
                     handleEleChange(e, "password");
                   }}
                   type="password"
                   value={formEle.password}
-                  placeholder="password"
+                  placeholder="John@123"
+                  className="form-textbox"
                 />
+              </div>
+              <div className="login-form-rememberme-forgot">
+                <label className="form-label-remember-me">
+                  <input
+                    className="form-checkbox-remember-me"
+                    type="checkbox"
+                  />{" "}
+                  Remember me
+                </label>
+                <div
+                  className="login-forgot-password"
+                  onClick={() => {
+                    alert("password reset");
+                  }}
+                >
+                  Forgot password?
+                </div>
               </div>
               <div className="login-form-btn-container">
                 <button
@@ -41,10 +70,12 @@ function Login({ handleSubmit, handleEleChange, formEle }) {
                     handleSubmit();
                   }}
                   type="submit"
+                  className="login-btn"
                 >
-                  Login
+                  Sign In
                 </button>
               </div>
+              {/* </div> */}
             </form>
           </div>
         </div>
